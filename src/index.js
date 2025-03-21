8ac01f32cdcde5e35c1f5bc7147277ebd946a26c
Jen-94 <jennifercsoto94@gmail.com>
2025-03-21 17:42:28 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 function generatePoem(event) {
   event.preventDefault();
+
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a very poetic AI assistant. You are capable of generate poems about any topic in a poetic, clever, elegant and family friendly manner</p>";
+  let prompt =
+    "Please generate a beautifil poem the way Pablo Neruda would do it";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   new Typewriter("#poem", {
     strings: "The poem goes here",
     autoStart: true,
