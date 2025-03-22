6f4c8c8f24b3660a634240176381ae2e907a0308
Jen-94 <jennifercsoto94@gmail.com>
2025-03-21 18:17:36 -0600

    Include API in Java

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
-  let context =
-    "You are a very poetic AI assistant. You are capable of generate poems about any topic in a poetic, clever, elegant and family friendly manner</p>";
-  let prompt =
-    "Please generate a beautifil poem the way Pablo Neruda would do it";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The poem goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1.5,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a very poetic AI assistant. You are capable of generate poems about any topic in a poetic, clever, elegant and family friendly manner</p>";
+  let prompt = ` User instructions: Generate a 5 line beautiful poem about ${instructionsInput.value}the way Pablo Neruda would do it and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element in the 6th line"`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
