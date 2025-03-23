ea83b27435667d58306d2b4edca196549760cffa
Jen-94 <jennifercsoto94@gmail.com>
2025-03-23 15:28:18 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<span class="generating"> Generating poem about  ${instructionsInput.value}...</span>`;
+  poemElement.innerHTML = `<div class="generating"> Generating poem about  ${instructionsInput.value}...</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
